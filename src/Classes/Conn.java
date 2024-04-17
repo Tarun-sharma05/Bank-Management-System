7a9d725439b36cfdef7dfca33af8f84dc6e8b485
Tarun Sharma <tarundadhich05@gmail.com>
2024-04-17 15:53:03 +0530

    second commit

--- src/Classes/Conn.java
+++ src/Classes/Conn.java
@@ -6,7 +6,7 @@ public class Conn {
     public Conn(){
         try{
            Class.forName("com.mysql.cj.jdbc.Driver");
-          c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem","root","Tarun-321");
+          c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem","root","root");
           s = c.createStatement();
         }catch (Exception e){
             System.out.println(e);
