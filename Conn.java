78f6f352a001fa0454d9868b580ce38a90365725
Tarun sharma <119731830+Tarun-sharma05@users.noreply.github.com>
2024-04-17 16:17:32 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,16 @@
+package Classes;
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+           Class.forName("com.mysql.cj.jdbc.Driver");
+          c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem","root","root");
+          s = c.createStatement();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
